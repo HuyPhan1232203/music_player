3652a0e472500fdce969a5594a27d4d50a6c8fee
Huy <Huyphan1232203@gmail.com>
2025-01-15 14:22:24 +0700

    update

---
+++ app/(tabs)/(song)/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { defaultStyles } from '@/styles/default'
+
+const SongsScreen = () => {
+  return (
+    <View style={defaultStyles.container}>
+        <Text>aldnaldnaskldnlaskndlasndlkasn</Text>
+    </View>
+  )
+}
+
+export default SongsScreen
